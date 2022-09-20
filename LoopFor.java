f7e44c1f40cd8cbd0695a50321ceb153ba83b164
JuvenMi <canercincin34@gmail.com>
2022-09-20 14:32:25 +0300

    Delete LoopFor.java

--- LoopFor.java
+++
@@ -1,8 +0,0 @@
-public class LoopFor {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-        System.out.println("End of for loop.");
-    }
-}
