2708e4d9fa99ba5b5b06df1467fe2cb4f9781384
JuvenMi <canercincin34@gmail.com>
2022-09-20 14:27:52 +0300

    Add files via upload

---
+++ Quiz1/Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    public static void main(String[] args) {
+        int studentNum = 10;
+        String message = "My class number is ";
+        System.out.println(message + studentNum);
+    }
+}
